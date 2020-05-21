1f95e0e4726ec57730aaabddba1b2c2be8c46786
Ben Drucker <bvdrucker@gmail.com>
2020-05-21 10:39:25 -0700

    add package doc

---
+++ doc.go
@@ -0,0 +1,15 @@
+// Package credentialhelper helps create a Terraform credential helper CLI.
+//
+// https://www.terraform.io/docs/internals/credentials-helpers.html
+//
+// It accepts a "Helper" interface that implements the get, store, and forget command
+// that Terraform will call. It prints output (or errors) to the correct output stream
+// and sets an exit code based on the result of the Helper. It also supports defining flags,
+// to be provided when the user defines their credential helper:
+//
+// credentials_helper "credstore" {
+//   args = ["--host=credstore.example.com"]
+// }
+//
+// Flags are accessible in a FlagSet passed to each Helper method.
+package credentialhelper
